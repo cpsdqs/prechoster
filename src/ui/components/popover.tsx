7499fe6a03f4e56983a5f55643d5cbbaf03c1f90
cpsdqs <cpsdqs@cloudwithlightning.net>
2022-07-28 12:38:38 +0200

    make popover nicer

--- src/ui/components/popover.tsx
+++ src/ui/components/popover.tsx
@@ -3,6 +3,10 @@ import { PureComponent } from 'preact/compat';
 import { AnimationController, Spring } from '../animation';
 import './popover.less';
 
+const WINDOW_MARGIN = 16;
+const ARROW_SIZE = 16;
+const ARROW_MARGIN = 10;
+
 export class Popover extends PureComponent<Popover.Props> {
     animCtrl = new AnimationController();
     presence = new Spring({
@@ -10,32 +14,118 @@ export class Popover extends PureComponent<Popover.Props> {
         stiffness: 439,
         damping: 31,
     });
+    snapToNextPosition = false;
+    anchorLoc = [0, 0];
+    arrowLoc: [string, number, number] = ['none', 0, 0];
+    positionX = new Spring({ stiffness: 439, damping: 42 });
+    positionY = new Spring({ stiffness: 439, damping: 42 });
     width = new Spring({ stiffness: 439, damping: 42 });
     height = new Spring({ stiffness: 439, damping: 42 });
     dialog = createRef();
     popover = createRef();
+    popoverContent: { current: HTMLElement | null } = { current: null };
+    popoverContentRef = (node: HTMLElement | null) => {
+        this.popoverContent.current = node;
+        if (node) this.resizeObserver.observe(node);
+    };
+
+    snapToNextContentSize = false;
+    resizeObserver = new ResizeObserver(entries => {
+        for (const entry of entries) {
+            // we're only observing one element
+            this.width.target = entry.contentRect.width;
+            this.height.target = entry.contentRect.height;
+
+            if (this.snapToNextContentSize) {
+                this.width.value = this.width.target;
+                this.height.value = this.height.target;
+                this.snapToNextContentSize = false;
+            }
+
+            this.animCtrl.add(this);
+        }
+    });
 
     update(dt: number) {
-        {
-            const popover = this.popover.current!;
-            const { width, height } = popover.style.width;
-            popover.style.width = '';
-            popover.style.height = '';
-            this.width.target = popover.offsetWidth;
-            this.height.target = popover.offsetHeight;
-            popover.style.width = this.width.value;
-            popover.style.height = this.height.value;
+        const popoverSize = [this.width.target, this.height.target];
+        const anchorMin = [window.innerWidth / 2, window.innerHeight / 2];
+        const anchorCenter = [window.innerWidth / 2, window.innerHeight / 2];
+        const anchorMax = [window.innerWidth / 2, window.innerHeight / 2];
+        if (this.props.anchor) {
+            const anchorRect = this.props.anchor.getBoundingClientRect();
+            anchorMin[0] = anchorRect.left;
+            anchorMin[1] = anchorRect.top;
+            anchorMax[0] = anchorRect.right;
+            anchorMax[1] = anchorRect.bottom;
+            anchorCenter[0] = anchorRect.left + anchorRect.width / 2;
+            anchorCenter[1] = anchorRect.top + anchorRect.height / 2;
         }
 
-        if (this.presence.value < 0.01) {
-            this.width.value = this.width.target;
-            this.height.value = this.height.target;
+        const popoverMinLoc = [WINDOW_MARGIN, WINDOW_MARGIN];
+        const popoverMaxLoc = [
+            window.innerWidth - popoverSize[0] - WINDOW_MARGIN,
+            window.innerHeight - popoverSize[1] - WINDOW_MARGIN,
+        ];
+
+        // default: just center it
+        let popoverLoc = [
+            anchorCenter[0] - popoverSize[0] / 2,
+            anchorCenter[1] - popoverSize[1] / 2,
+        ];
+        this.anchorLoc = anchorCenter;
+        let arrowLocType = 'none';
+
+        const hasEnoughSpaceAbove = anchorMin[1] - ARROW_SIZE - popoverSize[1] > popoverMinLoc[0];
+        const hasEnoughSpaceBelow = anchorMax[1] + ARROW_SIZE < popoverMaxLoc[1];
+        const hasEnoughSpaceOnRight = anchorMax[0] + ARROW_SIZE < popoverMaxLoc[0];
+        if (hasEnoughSpaceAbove) {
+            popoverLoc = [anchorCenter[0] - popoverSize[0] / 2, anchorMin[1] - ARROW_SIZE - popoverSize[1]];
+            this.anchorLoc = [anchorCenter[0], anchorMin[1]];
+            arrowLocType = 'bottom';
+        } else if (hasEnoughSpaceBelow) {
+            popoverLoc = [anchorCenter[0] - popoverSize[0] / 2, anchorMax[1] + ARROW_SIZE];
+            this.anchorLoc = [anchorCenter[0], anchorMax[1]];
+            arrowLocType = 'top';
+        } else if (hasEnoughSpaceOnRight) {
+            popoverLoc = [anchorMax[0] + ARROW_SIZE, anchorCenter[1] - popoverSize[1] / 2];
+            this.anchorLoc = [anchorMax[0], anchorCenter[1]];
+            arrowLocType = 'left';
+        }
+
+        popoverLoc[0] = Math.max(popoverMinLoc[0], Math.min(popoverLoc[0], popoverMaxLoc[0]));
+        popoverLoc[1] = Math.max(popoverMinLoc[1], Math.min(popoverLoc[1], popoverMaxLoc[1]));
+
+        this.positionX.target = popoverLoc[0];
+        this.positionY.target = popoverLoc[1];
+
+        if (!this.snapToNextContentSize && this.snapToNextPosition) {
+            // only snap to next position once content size has been resolved
+            this.positionX.value = this.positionX.target;
+            this.positionY.value = this.positionY.target;
+            this.snapToNextPosition = false;
         }
 
         let done = true;
         done = this.presence.update(dt) && done;
         done = this.width.update(dt) && done;
         done = this.height.update(dt) && done;
+        done = this.positionX.update(dt) && done;
+        done = this.positionY.update(dt) && done;
+
+        this.arrowLoc = ['none', 0, 0];
+        if (arrowLocType === 'left') {
+            const x = 0;
+            const y = this.anchorLoc[1] - this.positionY.value;
+            if (y > ARROW_MARGIN && y < this.height.value - ARROW_MARGIN) {
+                this.arrowLoc = ['left', x, y];
+            }
+        } else if (arrowLocType === 'top' || arrowLocType === 'bottom') {
+            const x = this.anchorLoc[0] - this.positionX.value;
+            const y = arrowLocType === 'top' ? 0 : this.height.value;
+            if (x > ARROW_MARGIN && x < this.width.value - ARROW_MARGIN) {
+                this.arrowLoc = [arrowLocType, x, y];
+            }
+        }
 
         this.forceUpdate();
 
@@ -46,6 +136,7 @@ export class Popover extends PureComponent<Popover.Props> {
             this.dialog.current.close();
         }
 
+
         return done;
     }
 
@@ -53,11 +144,27 @@ export class Popover extends PureComponent<Popover.Props> {
         this.dialog.current.addEventListener('close', this.onDialogClose);
         this.dialog.current.addEventListener('cancel', this.onDialogClose);
         window.addEventListener('resize', this.onResize);
+
+        if (this.props.open) {
+            this.presence.target = 1;
+            this.snapToNextContentSize = true;
+            this.snapToNextPosition = true;
+            this.animCtrl.add(this);
+        }
     }
 
     componentDidUpdate(prevProps: Popover.Props) {
         if (prevProps.open !== this.props.open) {
-            this.presence.target = this.props.open ? 1 : 0;
+            if (this.props.open) {
+                this.presence.target = 1;
+                if (this.presence.value < 0.01) {
+                    this.snapToNextContentSize = true;
+                    this.snapToNextPosition = true;
+                }
+            } else {
+                this.presence.target = 0;
+            }
+
             this.animCtrl.add(this);
         }
     }
@@ -78,18 +185,26 @@ export class Popover extends PureComponent<Popover.Props> {
     render({ anchor, children }: Popover.Props) {
         const presence = this.presence.value;
         const popoverOpacity = this.props.open ? 1 : this.presence.value;
+        const anchorLoc = this.anchorLoc;
+        const popoverLoc = [this.positionX.value, this.positionY.value];
+        const popoverTransform = `translate(${popoverLoc[0]}px, ${popoverLoc[1]}px) scale(${Math.max(0, presence)})`;
+        const popoverTransformOrigin = `${anchorLoc[0] - popoverLoc[0]}px ${anchorLoc[1] - popoverLoc[1]}px`;
+        let arrow = null;
 
-        const popoverSize = [this.width.target, this.height.target];
-        // just center it for now...
-        const popoverLoc = [
-            (window.innerWidth - popoverSize[0]) / 2,
-            (window.innerHeight - popoverSize[1]) / 2,
-        ];
-        const anchorLoc = [window.innerWidth / 2, window.innerHeight / 2];
-        if (anchor) {
-            const anchorRect = anchor.getBoundingClientRect();
-            anchorLoc[0] = anchorRect.left + anchorRect.width / 2;
-            anchorLoc[1] = anchorRect.top + anchorRect.height / 2;
+        if (this.arrowLoc[0] !== 'none') {
+            arrow = (
+                <div
+                    class="i-arrow"
+                    data-type={this.arrowLoc[0]}
+                    style={{
+                        transform: [
+                            popoverTransform,
+                            `translate(${this.arrowLoc[1]}px, ${this.arrowLoc[2]}px)`,
+                        ].join(' '),
+                        transformOrigin: popoverTransformOrigin,
+                        opacity: popoverOpacity,
+                    }} />
+            );
         }
 
         return (
@@ -104,12 +219,17 @@ export class Popover extends PureComponent<Popover.Props> {
                     style={{
                         width: this.width.value + 'px',
                         height: this.height.value + 'px',
-                        transform: `translate(${popoverLoc[0]}px, ${popoverLoc[1]}px) scale(${presence})`,
-                        transformOrigin: `${anchorLoc[0] - popoverLoc[0]}px ${anchorLoc[1] - popoverLoc[1]}px`,
+                        transform: popoverTransform,
+                        transformOrigin: popoverTransformOrigin,
                         opacity: popoverOpacity,
                     }}>
-                    {children}
+                    <div
+                        class="i-content"
+                        ref={this.popoverContentRef}>
+                        {children}
+                    </div>
                 </div>
+                {arrow}
             </dialog>
         );
     }
